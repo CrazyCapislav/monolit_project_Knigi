581874e2f34c148fb07286964e73caf36ecf2c8b
Rahim <rahim-2004@mail.ru>
2025-12-24 15:10:39 +0300

    Update OpenAPI configuration and service settings

--- backend/book-service/src/main/java/dev/petr/book/config/OpenApiConfig.java
+++ backend/book-service/src/main/java/dev/petr/book/config/OpenApiConfig.java
@@ -3,15 +3,23 @@ package dev.petr.book.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
+    @Value("${gateway.url:http://localhost:8080}")
+    private String gatewayUrl;
+
     @Bean
     public OpenAPI bookServiceOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(new Server().url(gatewayUrl).description("API Gateway")))
                 .info(new Info()
                         .title("Book Service API")
                         .description("Book and genre management service for BookSwap")
--- backend/exchange-service/src/main/java/dev/petr/exchange/config/OpenApiConfig.java
+++ backend/exchange-service/src/main/java/dev/petr/exchange/config/OpenApiConfig.java
@@ -3,15 +3,23 @@ package dev.petr.exchange.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
+    @Value("${gateway.url:http://localhost:8080}")
+    private String gatewayUrl;
+
     @Bean
     public OpenAPI exchangeServiceOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(new Server().url(gatewayUrl).description("API Gateway")))
                 .info(new Info()
                         .title("Exchange Service API")
                         .description("Book exchange management service for BookSwap")
